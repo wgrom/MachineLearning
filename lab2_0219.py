e43c5cf48207627089fd40ac6b78b8af8407ee87
waldemar <waldemar.gro@gmail.com>
2018-02-19 15:25:00 +0100

    reorder
    reorder of the prog

--- lab2_0219.py
+++ lab2_0219.py
@@ -8,64 +8,10 @@ Created on Wed Feb 14 17:46:40 2018
 import numpy , random , math
 from scipy.optimize import minimize 
 import matplotlib.pyplot as plt
-
-# ================================================== #
-# Preparing the Data and Plot
-# ================================================== #
-
-# set seed for testing:
-numpy.random.seed(100)
  
-## Creating Inputs
-classA = numpy.concatenate( 
-        (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
-         numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5])) 
-classB = numpy.random.randn(20, 2) * 0.2 + [0.0 , -0.5]
- 
-inputs = numpy.concatenate (( classA , classB )) 
-
-targets = numpy.concatenate (
-        (numpy.ones(classA.shape[0]) , 
-         -numpy.ones(classB.shape[0])))
- 
-# Number of rows (samples)
-global N 
-N = inputs.shape[0] 
- 
-# reorder the samples ramdomly
-permute= list(range(N)) 
-random.shuffle(permute) 
-inputs = inputs[ permute , : ]
-targets = targets[ permute ]
- 
-# plotting:
-def disp(ClassA,ClassB):
-    plt.plot([p[0] for p in classA ] ,
-             [p[1] for p in classA], 
-             'b.') 
-    plt.plot([p[0] for p in classB ] ,
-             [p[1] for p in classB], 
-             'r.')
-    plt.axis('equal') # Force same scale on both axes 
-    #plt.savefig(’svmplot.pdf’) # Save a copy in a file 
-    plt.show() # Show the plot on the screen 
-
- 
-
 # ================================================== #
 # Initialization of the Program
 # ================================================== #
-#Start values for alfa vector
-#N - number of training samples, each has corresp. alfa
-start = numpy.zeros(N)
- 
-# list of pairs as long as alfa
-C=1
-bounds = [(0, C) for b in range(N)]
-
-# XC equality constraint of (10)
-#constraint={'type': eq, 'fun':zerofun}
-
 
 ## Functions
 global c
@@ -84,7 +30,7 @@ def FormingP(x,y):
     P =numpy.zeros((N,N))    # Initialization of Matrix
     for a in range(N):
         for b in range(N):
-            P[a,b] = x[a]*x[b]*kernel(y[a],y[b],c) #1 is for kernel selection
+            P[a,b] = x[a]*x[b]*kernel(y[a],y[b],c) #c is for kernel selection
     
 def objective(alpha):
     v = numpy.zeros(N,N)
@@ -99,7 +45,6 @@ def zerofun(alpha,t):
     return numpy.sum(v)
 
 threshold = 0.00001
-
 def extractSupportVectors(testdata, targetdata, alphaValues):
     supportVectors = []
     for i in range(len(testdata)):
@@ -121,20 +66,65 @@ def threshold_value(supportVectors,x):
     for i in range(len(supportVectors))]
     return b
 
+# plotting:
+def disp(ClassA,ClassB):
+    plt.plot([p[0] for p in classA ] ,
+             [p[1] for p in classA], 
+             'b.') 
+    plt.plot([p[0] for p in classB ] ,
+             [p[1] for p in classB], 
+             'r.')
+    plt.axis('equal') # Force same scale on both axes 
+    #plt.savefig(’svmplot.pdf’) # Save a copy in a file 
+    plt.show() # Show the plot on the screen 
 
+# ================================================== #
+# Preparing the Data
+# ================================================== #
+
+# set seed for testing:
+numpy.random.seed(100) 
+## Creating Inputs
+classA = numpy.concatenate( 
+        (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
+         numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5])) 
+classB = numpy.random.randn(20, 2) * 0.2 + [0.0 , -0.5]
+ 
+inputs = numpy.concatenate (( classA , classB )) 
+
+targets = numpy.concatenate (
+        (numpy.ones(classA.shape[0]) , 
+         -numpy.ones(classB.shape[0])))
+ 
+# Number of rows (samples)
+global N 
+N = inputs.shape[0] 
+ 
+# reorder the samples ramdomly
+permute= list(range(N)) 
+random.shuffle(permute) 
+inputs = inputs[ permute , : ]
+targets = targets[ permute ]
+
+#Start values for alfa vector
+#N - number of training samples, each has corresp. alfa
+start = numpy.zeros(N)
+ 
+# list of pairs as long as alfa
+C=1
+bounds = [(0, C) for b in range(N)]
+# XC equality constraint of (10)
+#constraint={'type': eq, 'fun':zerofun}
 
 
 # ================================================== #
 # Main Program
-# ================================================== #    
+# ================================================== #   
 
-    
-            
-            
-            
-ret = minimize( objective , start , bounds=B, constraints=XC)
-alpha = ret['x']
-solution = ret['success']
+#def minimize( objective , start , bounds=B, constraints=XC):
+#    alpha = ret['x']    
+#    solution = ret['success']
+
 
 
 
