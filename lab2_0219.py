e4797798da98610b1125c90ea2c97d8801f889b9
SImon <heidsimon@gmail.com>
2018-02-19 15:35:23 +0100

    Creating Functions
    Working till Ploting Boundaries

--- lab2_0219.py
+++ lab2_0219.py
@@ -13,30 +13,30 @@ import matplotlib.pyplot as plt
 # Preparing the Data and Plot
 # ================================================== #
 
-# set seed for testing:
-numpy.random.seed(100)
- 
-## Creating Inputs
-classA = numpy.concatenate( 
-        (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
-         numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5])) 
-classB = numpy.random.randn(20, 2) * 0.2 + [0.0 , -0.5]
- 
-inputs = numpy.concatenate (( classA , classB )) 
-
-targets = numpy.concatenate (
-        (numpy.ones(classA.shape[0]) , 
-         -numpy.ones(classB.shape[0])))
- 
-# Number of rows (samples)
-global N 
-N = inputs.shape[0] 
- 
-# reorder the samples ramdomly
-permute= list(range(N)) 
-random.shuffle(permute) 
-inputs = inputs[ permute , : ]
-targets = targets[ permute ]
+def GeneratingData():
+    # set seed for testing:
+    numpy.random.seed(100)
+     
+    ## Creating Input
+    global classA, classB, inputs,targets
+    classA = numpy.concatenate( 
+            (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
+             numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5])) 
+    classB = numpy.random.randn(20, 2) * 0.2 + [0.0 , -0.5]
+    inputs = numpy.concatenate (( classA , classB )) 
+    targets = numpy.concatenate (
+            (numpy.ones(classA.shape[0]) , 
+             -numpy.ones(classB.shape[0])))
+     
+    # Number of rows (samples)
+    global N 
+    N = inputs.shape[0] 
+     
+    # reorder the samples ramdomly
+    permute= list(range(N)) 
+    random.shuffle(permute) 
+    inputs = inputs[ permute , : ]
+    targets = targets[ permute ]
  
 # plotting:
 def disp(ClassA,ClassB):
@@ -49,6 +49,14 @@ def disp(ClassA,ClassB):
     plt.axis('equal') # Force same scale on both axes 
     #plt.savefig(’svmplot.pdf’) # Save a copy in a file 
     plt.show() # Show the plot on the screen 
+    
+    
+def dispBoundaries(supportVec):
+    xgrid=numpy.linspace(-5, 5) 
+    ygrid=numpy.linspace(-4, 4)
+    grid=numpy.array([[indicator(x, y,supportVec) for y in ygrid ] for x in xgrid])
+    plt.contour (xgrid , ygrid , grid , (-1.0, 0.0, 1.0),
+             colors=('red', 'black', 'blue'), linewidths=(1, 3, 1))
 
  
 
@@ -57,14 +65,7 @@ def disp(ClassA,ClassB):
 # ================================================== #
 #Start values for alfa vector
 #N - number of training samples, each has corresp. alfa
-start = numpy.zeros(N)
- 
-# list of pairs as long as alfa
-C=1
-bounds = [(0, C) for b in range(N)]
 
-# XC equality constraint of (10)
-#constraint={'type': eq, 'fun':zerofun}
 
 
 ## Functions
@@ -79,24 +80,25 @@ def kernel(x,y,c):
         param = 0.01
         return math.exp**(math.pow(numpy.linalg.norm(x-y),2)/(2*math.pow(param,2)))
 
-def FormingP(x,y):
+def FormingP(t,x):
     global P
     P =numpy.zeros((N,N))    # Initialization of Matrix
     for a in range(N):
         for b in range(N):
-            P[a,b] = x[a]*x[b]*kernel(y[a],y[b],c) #1 is for kernel selection
+            P[a,b] = t[a]*t[b]*kernel(x[a],x[b],c) #1 is for kernel selection
     
 def objective(alpha):
-    v = numpy.zeros(N,N)
+    v = numpy.zeros((N,N))
     for a in  range(N):
         for b in range(N):
             v[a,b]= 1/2*numpy.dot(alpha[a],alpha[b])*P[a,b]
     return numpy.sum(v)-numpy.sum(alpha)
     
-def zerofun(alpha,t):
+def zerofun(alpha):
+    v = 0
     for a in range(N):
-        v = numpy.dot(alpha[a],t[a])
-    return numpy.sum(v)
+        v += numpy.dot(alpha[a],targets[a])
+    return v
 
 threshold = 0.00001
 
@@ -107,7 +109,7 @@ def extractSupportVectors(testdata, targetdata, alphaValues):
             supportVectors.append((alphaValues[i], targetdata[i], testdata[i]))
     return supportVectors
 
-def indicator(xVec, supportVec):
+def indicator(xVec, yVec, supportVec):
     result =0
     for a in range(len(supportVec)):
         alphaData, targetData, testData = supportVec[a]
@@ -117,7 +119,7 @@ def indicator(xVec, supportVec):
     return result
 
 def threshold_value(supportVectors,x):
-    b=[numpy.sum(supportVectors[i][0]*supportVectors[i][1]*kernel(supportVectors[i][2],x[i],c)-supportVectors[i][1]) 
+    b=[numpy.sum(supportVectors[i][0]*supportVectors[i][1]*kernel(supportVectors[0][2],x[i],c)-supportVectors[0][1]) 
     for i in range(len(supportVectors))]
     return b
 
@@ -127,22 +129,30 @@ def threshold_value(supportVectors,x):
 # ================================================== #
 # Main Program
 # ================================================== #    
-
+def run():
+        
+    GeneratingData()
+    disp(classA,classB)
+    # Parameters for minimize
+    start = numpy.zeros(N)
+    C=1
+    B = [(0, None) for b in range(N)]
+    XC={'type': 'eq', 'fun':zerofun}
     
-            
-            
-            
-ret = minimize( objective , start , bounds=B, constraints=XC)
-alpha = ret['x']
-solution = ret['success']
+    
+    FormingP(targets,inputs)
+    
+    ret = minimize( objective , start , bounds=B, constraints=XC)
+    alpha = ret['x']
+    #solution = ret['success']
+    
+    supportVec = extractSupportVectors(inputs,targets,alpha)
+    
+    dispBoundaries(supportVec)
 
 
 
-xgrid=numpy.linspace(-5, 5) 
-ygrid=numpy.linspace(-4, 4)
-grid=numpy.array([[indicator(x, y) for y in ygrid ] for x in xgrid])
-plt.contour (xgrid , ygrid , grid , (-1.0, 0.0, 1.0),
-             colors=('red', 'black', 'blue'), linewidths=(1, 3, 1))
+
 
     
 
